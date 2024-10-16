a2a89f7f28f91b0a704d744a5f10f6804f382fb4
bilal awad <s320760@studenti.polito.it>
2024-10-16 12:47:47 +0200

    update2

--- Lab1/exercise_set1.py
+++ Lab1/exercise_set1.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib as plt
+from prompt_toolkit.key_binding.bindings.named_commands import end_kbd_macro
 
 
 # Please, remove all the pass in the exercises and substitute them with the expected methods for your functions
@@ -35,7 +36,13 @@ def exercise5():
 
 # ex6
 def exercise6():
-    pass
+    counter=0
+    trial=input("enter a string")
+    for word in trial.split():
+        if word=="emma":
+            counter+=1
+    print(counter)
+
 
 
 # ex7
@@ -70,27 +77,4 @@ def exercise12():
 
 if __name__ == "__main__":
     print("EXERCISE SET 1")
-    print("EX1")
-    exercise1()
-    print("EX2")
-    exercise2()
-    print("EX3")
-    exercise3()
-    print("EX4")
-    exercise4()
-    print("EX5")
-    exercise5()
-    print("EX6")
     exercise6()
-    print("EX7")
-    exercise7()
-    print("EX8")
-    exercise8()
-    print("EX9")
-    exercise9()
-    print("EX10")
-    exercise10()
-    print("EX11")
-    exercise11()
-    print("EX12")
-    exercise12()
